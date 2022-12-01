2909a43c63dc0fc6c4ba7ba322c9e53e7f69683f
Bartosz Przybysz <bartekprzybysz@gmail.com>
2022-12-01 18:27:09 +0100

    swiper done but non RWD

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// SWIPER
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
@@ -20,3 +20,43 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
+
+
+// Graphic carousel buttons
+function changeDisplay1() {
+    const myDiv1 = document.getElementById("carousel-graphics");
+    const myDiv2 = document.getElementById("carousel-screenshots");
+    const myDiv3 = document.getElementById("carousel-wallpapers");
+    if (myDiv1.style.display = "none") {
+        myDiv1.style.display = "block";
+        myDiv2.style.display = "none";
+        myDiv3.style.display = "none";
+    }
+}
+
+function changeDisplay2() {
+    const myDiv1 = document.getElementById("carousel-graphics");
+    const myDiv2 = document.getElementById("carousel-screenshots");
+    const myDiv3 = document.getElementById("carousel-wallpapers");
+    if (myDiv2.style.display = "none") {
+        myDiv2.style.display = "block";
+        myDiv1.style.display = "none";
+        myDiv3.style.display = "none";
+    }
+}
+
+function changeDisplay3() {
+    const myDiv1 = document.getElementById("carousel-graphics");
+    const myDiv2 = document.getElementById("carousel-screenshots");
+    const myDiv3 = document.getElementById("carousel-wallpapers");
+    if (myDiv3.style.display = "none") {
+        myDiv3.style.display = "block";
+        myDiv1.style.display = "none";
+        myDiv2.style.display = "none";
+    }
+}
+
+// All buttons links to Zombie
+function visitPage() {
+    window.location = 'zombie.html';
+}
